e076521061ee52974b4a755f9c4aeb6d55b16fee
JulianoTitarelli <julianotitarelli@outlook.com>
2026-08-01 11:48:13 -0300

    Update produtos.js

--- dados/produtos.js
+++ dados/produtos.js
@@ -1,44 +1,129 @@
 const produtos = [
+
+    // ☕ CAFETERIA
+
     {
         id: 1,
-        nome: "Água Mineral",
-        categoria: "Bebidas",
-        preco: 5.00
+        nome: "Café expresso DolceGusto 50ml",
+        categoria: "Cafeteria",
+        preco: 9.00
     },
+
     {
         id: 2,
-        nome: "Refrigerante",
-        categoria: "Bebidas",
+        nome: "Café coado",
+        categoria: "Cafeteria",
         preco: 8.00
     },
+
     {
         id: 3,
-        nome: "Coca-Cola Lata",
-        categoria: "Bebidas",
-        preco: 7.00
+        nome: "Café espresso DolceGusto Lungo",
+        categoria: "Cafeteria",
+        preco: 9.00
     },
+
     {
         id: 4,
-        nome: "X-Salada",
-        categoria: "Lanches",
-        preco: 25.00
+        nome: "Café espresso DolceGusto Matinal",
+        categoria: "Cafeteria",
+        preco: 9.00
     },
+
     {
         id: 5,
-        nome: "X-Bacon",
-        categoria: "Lanches",
-        preco: 30.00
+        nome: "Café espresso DolceGusto Doppio",
+        categoria: "Cafeteria",
+        preco: 9.00
     },
+
     {
         id: 6,
-        nome: "Batata Frita",
-        categoria: "Porções",
-        preco: 18.00
+        nome: "Espresso Latte Nesquik",
+        categoria: "Cafeteria",
+        preco: 11.00
     },
+
     {
         id: 7,
-        nome: "Pudim",
-        categoria: "Sobremesas",
-        preco: 10.00
-    }
-];
+        nome: "Espresso Latte KitKat",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 8,
+        nome: "Espresso Au Lait",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 9,
+        nome: "Espresso Latte Tea Chai",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 10,
+        nome: "Espresso Latte Língua de Gato",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 11,
+        nome: "Cappuccino Tradicional",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 12,
+        nome: "Cappuccino Caramelo Salgado",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 13,
+        nome: "Cappuccino Doce de Leite",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 14,
+        nome: "Cappuccino Brigadeiro",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 15,
+        nome: "Cappuccino Canela",
+        categoria: "Cafeteria",
+        preco: 11.00
+    },
+
+    {
+        id: 16,
+        nome: "Cappuccino Borda de Creme de Avelã",
+        categoria: "Cafeteria",
+        preco: 14.00
+    },
+
+    {
+        id: 17,
+        nome: "Frappé",
+        categoria: "Cafeteria",
+        preco: 14.00
+    },
+
+    {
+        id: 18,
+        nome: "Affogato",
+        categoria: "Cafeteria",
+        preco: 12.00
+    },
