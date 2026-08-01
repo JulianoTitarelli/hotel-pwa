0f3534b9ccd43d90fdbded55cb130a9ed241d7c5
JulianoTitarelli <julianotitarelli@outlook.com>
2026-08-01 07:42:09 -0300

    Update whatsapp.js

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,1 +1,57 @@
+function enviarPedido(){
 
+    if(carrinho.length === 0){
+
+        alert("Adicione algum produto ao pedido.");
+
+        return;
+
+    }
+
+
+    const quarto = document.getElementById("quarto").value;
+
+
+    if(quarto === ""){
+
+        alert("Informe o número do quarto.");
+
+        return;
+
+    }
+
+
+    let mensagem = 
+`📦 NOVO PEDIDO
+
+🏨 Hotel do Baú
+
+🚪 Quarto: ${quarto}
+
+`;
+
+
+    carrinho.forEach(item => {
+
+        mensagem += 
+`🍽️ ${item.nome} x ${item.quantidade}
+`;
+
+    });
+
+
+    mensagem += 
+`
+💰 Total: R$ ${total.innerHTML}
+`;
+
+
+    const texto = encodeURIComponent(mensagem);
+
+
+    window.open(
+        "https://wa.me/?text=" + texto,
+        "_blank"
+    );
+
+}
