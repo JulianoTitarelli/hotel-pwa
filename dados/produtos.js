840efdb9d36ab45dfb0bcc76656dfae4dcd1309b
JulianoTitarelli <julianotitarelli@outlook.com>
2026-08-01 11:48:48 -0300

    Update produtos.js

--- dados/produtos.js
+++ dados/produtos.js
@@ -127,3 +127,186 @@ const produtos = [
         categoria: "Cafeteria",
         preco: 12.00
     },
+// 🥐 SALGADOS
+
+{
+    id: 19,
+    nome: "Salgado assado tortinha de costela",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 20,
+    nome: "Salgado assado esfiha de carne",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 21,
+    nome: "Salgado assado esfiha de frango",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 22,
+    nome: "Salgado assado ghirella de pizza",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 23,
+    nome: "Salgado assado presunto e queijo",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 24,
+    nome: "Salgado assado X-Burguer com bacon",
+    categoria: "Salgados",
+    preco: 10.00
+},
+
+{
+    id: 25,
+    nome: "Croissant pizza",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 26,
+    nome: "Croissant frango com requeijão",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 27,
+    nome: "Croissant de chocolate",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+{
+    id: 28,
+    nome: "Croissant de doce de leite",
+    categoria: "Salgados",
+    preco: 7.50
+},
+
+
+// 🥪 LANCHES
+
+{
+    id: 29,
+    nome: "Lanche de carne louca",
+    categoria: "Lanches",
+    preco: 23.00
+},
+
+{
+    id: 30,
+    nome: "Lanche de ragu",
+    categoria: "Lanches",
+    preco: 25.00
+},
+
+{
+    id: 31,
+    nome: "Lanche de frango empanado",
+    categoria: "Lanches",
+    preco: 22.00
+},
+
+{
+    id: 32,
+    nome: "Lanche natural",
+    categoria: "Lanches",
+    preco: 14.00
+},
+
+{
+    id: 33,
+    nome: "Misto quente",
+    categoria: "Lanches",
+    preco: 15.00
+},
+
+
+// 🍽️ PRATOS FEITOS
+
+{
+    id: 34,
+    nome: "Nhoque com molho vermelho",
+    categoria: "Pratos feitos",
+    preco: 19.90
+},
+
+{
+    id: 35,
+    nome: "Nhoque com molho bechamel",
+    categoria: "Pratos feitos",
+    preco: 19.90
+},
+
+{
+    id: 36,
+    nome: "Salada Grande",
+    categoria: "Pratos feitos",
+    preco: 10.00
+},
+
+{
+    id: 37,
+    nome: "Arroz biro-biro",
+    categoria: "Pratos feitos",
+    preco: 21.90
+},
+
+{
+    id: 38,
+    nome: "Arroz carreteiro light",
+    categoria: "Pratos feitos",
+    preco: 29.90
+},
+
+{
+    id: 39,
+    nome: "Estrogonofe de frango",
+    categoria: "Pratos feitos",
+    preco: 27.90
+},
+
+{
+    id: 40,
+    nome: "Estrogonofe de carne",
+    categoria: "Pratos feitos",
+    preco: 29.90
+},
+
+{
+    id: 41,
+    nome: "Arroz com feijão e frango empanado",
+    categoria: "Pratos feitos",
+    preco: 21.90
+},
+
+{
+    id: 42,
+    nome: "Arroz com feijão e ragú ou lagarto",
+    categoria: "Pratos feitos",
+    preco: 22.90
+},
+
+{
+    id: 43,
+    nome: "Arroz com feijão e ovo",
+    categoria: "Pratos feitos",
+    preco: 19.90
+},
+    ];
