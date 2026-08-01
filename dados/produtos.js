c8924e688f8974c194b2a230faf7ff5b454cc44f
JulianoTitarelli <julianotitarelli@outlook.com>
2026-08-01 07:39:28 -0300

    Update produtos.js

--- dados/produtos.js
+++ dados/produtos.js
@@ -1,1 +1,44 @@
-
+const produtos = [
+    {
+        id: 1,
+        nome: "Água Mineral",
+        categoria: "Bebidas",
+        preco: 5.00
+    },
+    {
+        id: 2,
+        nome: "Refrigerante",
+        categoria: "Bebidas",
+        preco: 8.00
+    },
+    {
+        id: 3,
+        nome: "Coca-Cola Lata",
+        categoria: "Bebidas",
+        preco: 7.00
+    },
+    {
+        id: 4,
+        nome: "X-Salada",
+        categoria: "Lanches",
+        preco: 25.00
+    },
+    {
+        id: 5,
+        nome: "X-Bacon",
+        categoria: "Lanches",
+        preco: 30.00
+    },
+    {
+        id: 6,
+        nome: "Batata Frita",
+        categoria: "Porções",
+        preco: 18.00
+    },
+    {
+        id: 7,
+        nome: "Pudim",
+        categoria: "Sobremesas",
+        preco: 10.00
+    }
+];
