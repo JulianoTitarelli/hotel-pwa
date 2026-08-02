df67228f91acccdb0fa7b2a1ff5710bb9b0d4b5d
JulianoTitarelli <julianotitarelli@outlook.com>
2026-08-02 10:31:41 -0300

    Update cardapio.js

--- js/cardapio.js
+++ js/cardapio.js
@@ -15,7 +15,7 @@ const busca = document.getElementById("buscar");
 const imagensCategoria = {
     "Salgados": "img/salgados.webp",
     "Lanches": "img/lanche.webp",
-    "Pratos Feitos": "img/pf.webp",
+    "Pratos Feitos": "img/lanche.webp",
     "Doces": "img/sobremesas.webp",
     "Petiscos": "img/petiscos.webp"
 
